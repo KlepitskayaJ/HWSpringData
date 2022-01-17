4d0474b2414e4a895c1460c368640ff7955224d6
Klepitskaya <jenka1330.gmail.com>
2022-01-17 23:12:05 +0300

    last commit

--- src/main/java/com/ita/u1/hw/Config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.ita.u1.hw.Config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
